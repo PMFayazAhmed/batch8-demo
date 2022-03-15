332faf75bf100bfe7ca27f5f63f36d0d63312cff
Fayaz <fana.fayaz@gmail.com>
2022-03-15 19:27:24 +0530

    Created new file Sample.java in Mydir difrectory and modified Addition.java

--- Addition.java
+++ Addition.java
@@ -6,6 +6,6 @@ int num1,num2,res;
 num1=20;
 num2=45;
 res=num1+num2;
-System.out.println("The result is "+res);
+System.out.println("The sum is "+res);
 }
 }
---
+++ Mydir/Sample.java
@@ -0,0 +1,8 @@
+class Sample
+{
+public static void main(String args[])
+{
+
+System.out.println("Hello Welcome to DEVOPS ONLINE TRAINING");
+}
+}

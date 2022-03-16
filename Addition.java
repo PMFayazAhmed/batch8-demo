a3014cccd2d74437b2373945334dc196d5eceda2
Fayaz <fana.fayaz@gmail.com>
2022-03-16 07:43:14 +0530

    Modified num1 & numw2 values

--- Addition.java
+++ Addition.java
@@ -3,8 +3,8 @@ class Addition
 public static void main(String args[])
 {
 int num1,num2,res;
-num1=2005;
-num2=4505;
+num1=20050;
+num2=45050;
 res=num1+num2;
 System.out.println("The sum is "+res);
 //This is a comment
